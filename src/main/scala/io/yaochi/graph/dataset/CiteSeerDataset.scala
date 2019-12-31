f7409408280840d5e6ebfa5fb08b4bf5fc97fd69
yaochi <yaochi@sugo.io>
2019-12-31 18:43:13 +0800

    add empty methods for complex dataset

---
+++ src/main/scala/io/yaochi/graph/dataset/CiteSeerDataset.scala
@@ -0,0 +1,9 @@
+package io.yaochi.graph.dataset
+
+import org.apache.spark.sql.DataFrame
+
+object CiteSeerDataset {
+  def load(directory: String): (DataFrame, DataFrame) = {
+    null
+  }
+}
---
+++ src/main/scala/io/yaochi/graph/dataset/PubmedDataset.scala
@@ -0,0 +1,9 @@
+package io.yaochi.graph.dataset
+
+import org.apache.spark.sql.DataFrame
+
+object PubmedDataset {
+  def load(directory: String): (DataFrame, DataFrame) = {
+    null
+  }
+}
